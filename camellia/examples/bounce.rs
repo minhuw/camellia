d3df32bf85aa5b7a667400433a94fefba599d63e
minhuw <wangmh15@gmail.com>
2024-06-15 16:25:36 +0000

    feat: add a bounce example

---
+++ camellia/examples/bounce.rs
@@ -0,0 +1,45 @@
+use camellia::{
+    socket::af_xdp::XskSocketBuilder,
+    umem::{base::UMemBuilder, shared::SharedAccessorRef},
+};
+use clap::Parser;
+use std::sync::{Arc, Mutex};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    nic: String,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    println!("{}", cli.nic);
+
+    let umem = Arc::new(Mutex::new(
+        UMemBuilder::new().num_chunks(16384).build().unwrap(),
+    ));
+
+    let socket_builder = XskSocketBuilder::<SharedAccessorRef>::new()
+        .ifname(&cli.nic)
+        .queue_index(0)
+        .with_umem(umem.clone())
+        .enable_cooperate_schedule();
+
+    let mut socket = socket_builder.build_shared().unwrap();
+    const BATCH_SIZE: usize = 32;
+    loop {
+        let frames = socket.recv_bulk(BATCH_SIZE).unwrap();
+        let frames: Vec<_> = frames
+            .into_iter()
+            .map(|frame| {
+                let (mut ether_header, _remaining) =
+                    etherparse::Ethernet2Header::from_slice(frame.raw_buffer()).unwrap();
+
+                std::mem::swap(&mut ether_header.source, &mut ether_header.destination);
+                frame
+            })
+            .collect();
+
+        socket.send_bulk(frames).unwrap();
+    }
+}
